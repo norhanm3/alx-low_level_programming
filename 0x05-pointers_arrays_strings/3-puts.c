548631b9ee7c86b5660f85febc3e76f695243f60
norhanm3 <norhanegypt37@gmail.com>
2023-03-27 16:18:50 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -11,3 +11,17 @@ _putchar(*str++);
 }
 _putchar('\n');
 }
+#include <stdio.h>
+void _puts(char *);
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char *str;
+str = "Holberton!";
+_puts(str);
+return (0);
+}
