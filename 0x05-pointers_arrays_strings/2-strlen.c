a5acfef328c0b05c0e7866fcc8882e355df6b59e
norhanm3 <norhanegypt37@gmail.com>
2023-03-27 15:47:04 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+/**
+ * _strlen - Computes the length of a string.
+ *
+ * @s: Pointer to the string to be measured.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+int length = 0;
+while (*s != '\0')
+{
+length++;
+s++;
+}
+return (length);
+}
